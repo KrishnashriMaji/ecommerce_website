91b30f9a3eae6b959a37295ebef3c6e14836992c
KrishnashriMaji <krishnashrimaji@yahoo.com>
2021-05-05 23:16:18 +0530

    first

--- frontend/src/component/LoaderBox.js
+++ frontend/src/component/LoaderBox.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function LoaderBox() {
     return (
         <div>
-            <i className="fa fa-spinner fa-spin"></i>Loading...2
+            <i className="fa fa-spinner fa-spin"></i>Loading...2222
         </div>
     )
 }
